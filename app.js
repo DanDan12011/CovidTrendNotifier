69d9c0abb51fccb4806e46ef81d089de3b093e0f
DanDan12011 <101851009+DanDan12011@users.noreply.github.com>
2022-09-25 12:49:00 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var myForm = document.getElementById('myForm');
+
+myForm.addEventListener('submit', function(e){;
+
+    var country = document.getElementById('Country').value;
+    alert(country);
+
+});
